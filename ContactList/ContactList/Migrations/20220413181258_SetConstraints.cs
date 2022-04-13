a6d12bb14b0c8d05d14558d4b22087288b6b7b56
Andrei Androne <andrei.androne@outlook.com>
2022-04-13 21:28:55 +0300

    Lab30_FirstLookWebPage

---
+++ ContactList/ContactList/Migrations/20220413181258_SetConstraints.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactList.Migrations
+{
+    public partial class SetConstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "ContactLists",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactLists",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ContactLists",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "ContactLists",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "ContactLists",
+                newName: "ID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactLists",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ContactLists",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Address",
+                table: "ContactLists",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500,
+                oldNullable: true);
+        }
+    }
+}
---
+++ ContactList/ContactList/Models/ContactListEntry.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactList.Models
+{
+    //[Table("ContactLists")]
+    public class ContactListEntry
+    {
+        [Key]
+        public int Id { get; set; }
+        public ContactType ContactType { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? DateOfBirth { get; set; }
+
+        [MaxLength(500)]
+        public string Address { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [MaxLength(50)]
+        public string PhoneNumber { get; set; }
+
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
